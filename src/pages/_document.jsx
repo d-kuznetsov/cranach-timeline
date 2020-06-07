d19127bb7a18f5515d096f941350617efd117efd
Dmitrii Kuznetcov <Dmitrii.Kuznetcov@green-connector.de>
2020-06-07 17:17:48 +0200

    override _document to add css in js server side support

---
+++ src/pages/_document.jsx
@@ -0,0 +1,27 @@
+import Document from "next/document";
+import { ServerStyleSheets } from "@material-ui/core/styles";
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const sheets = new ServerStyleSheets();
+    const originalRenderPage = ctx.renderPage;
+
+    ctx.renderPage = () =>
+      originalRenderPage({
+        enhanceApp: (App) => (props) => sheets.collect(<App {...props} />),
+      });
+
+    const initialProps = await Document.getInitialProps(ctx);
+    return {
+      ...initialProps,
+      styles: (
+        <>
+          {initialProps.styles}
+          {sheets.getStyleElement()}
+        </>
+      ),
+    };
+  }
+}
+
+export default MyDocument;
