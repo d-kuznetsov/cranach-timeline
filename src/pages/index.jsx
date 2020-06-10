d41997f42254ea9e5a6f6b123c966eb05a505991
Dmitry Kuznetsov <ds.kuznetsov93@yandex.ru>
2020-06-10 09:41:09 +0200

    to test only

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,5 +1,24 @@
 import Layout from "../components/Layout";
-
+import RangeSlider from "../components/RangeSlider";
+import TimeLine from "../components/TimeLine";
+import Cat from "../components/Categories";
+import { loadArtworks } from "../redux/actions";
+import { useSelector, useDispatch } from "react-redux";
+import { useEffect } from "react";
 export default function Home() {
-  return <Layout />;
+  const artworks = useSelector((state) => state.artworksToView);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(loadArtworks());
+  }, [0]);
+
+  return (
+    <Layout>
+      <div>
+        <Cat />
+        <TimeLine />
+        <RangeSlider />
+      </div>
+    </Layout>
+  );
 }
