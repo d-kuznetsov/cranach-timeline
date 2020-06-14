bd61680c4d5c57ee3e46d062776ab7b452a7042b
Dmitry Kuznetsov <ds.kuznetsov93@yandex.ru>
2020-06-15 00:10:43 +0200

    use special component

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,24 +1,5 @@
-import Layout from "../components/Layout";
-import { RangeSliderContainer as RangeSlider } from "../components/RangeSlider";
-import { TimelineContainer as Timeline } from "../components/TimeLine";
-import { CategoryContainer as Categories } from "../components/Categories";
-import { loadArtworks } from "../redux/actions";
-import { useSelector, useDispatch } from "react-redux";
-import { useEffect } from "react";
-export default function Home() {
-  const artworks = useSelector((state) => state.artworksToView);
-  const dispatch = useDispatch();
-  useEffect(() => {
-    dispatch(loadArtworks());
-  }, [0]);
+import Timeline from "../page containers/Timeline";
 
-  return (
-    <Layout>
-      <div>
-        <Categories />
-        <Timeline />
-        <RangeSlider />
-      </div>
-    </Layout>
-  );
+export default function Home() {
+  return <Timeline />;
 }
