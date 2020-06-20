e29dea0d4f647485015707df429d0a9a8f83ddae
Dmitry Kuznetsov <ds.kuznetsov93@yandex.ru>
2020-06-20 23:28:26 +0200

    add components to panel

--- src/pages/timeline.jsx
+++ src/pages/timeline.jsx
@@ -1,5 +1,8 @@
 import Layout from "../components/Layout";
 import { RangeSliderContainer as RangeSlider } from "../components/RangeSlider";
+import ExpansionPanel from "../components/ExpansionPanel";
+import { CategoryContainer as Categories } from "../components/Categories";
+import { LineHeightSliderContainer as LineHeightSlider } from "../components/LineHeightSlider";
 import { LegendContainer as Legend } from "../components/Legend";
 import { TimelineContainer as Timeline } from "../components/TimeLine";
 import styles from "../styles/pages/TimelinePage.module.scss";
@@ -9,7 +12,16 @@ export default function TimelinePage() {
     <Layout
       toolbar={
         <div>
-          <RangeSlider />
+          <ExpansionPanel summary={<RangeSlider />}>
+            <section className={styles.expansionPanel}>
+              <section className={styles.categories}>
+                <Categories />
+              </section>
+              <section className={styles.lineHeightSlider}>
+                <LineHeightSlider />
+              </section>
+            </section>
+          </ExpansionPanel>
           <Legend />
         </div>
       }
