fd2b3c63d5b28edfc405d54724dbd80c84c6f36a
Dmitry Kuznetsov <ds.kuznetsov93@yandex.ru>
2020-06-20 12:59:12 +0200

    add grid component

--- src/pages/grid.jsx
+++ src/pages/grid.jsx
@@ -1,6 +1,11 @@
 import Layout from "../components/Layout";
+import { GridContainer as Grid } from "../components/Grid";
 import styles from "../styles/pages/GridPage.module.scss";
 
 export default function GridPage() {
-  return <Layout>Grid</Layout>;
+  return (
+    <Layout toolbar={"toolbar"}>
+      <Grid />
+    </Layout>
+  );
 }
