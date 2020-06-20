0c0a6177b459aa22c4b682a155f65d6be2f892fd
Dmitry Kuznetsov <ds.kuznetsov93@yandex.ru>
2020-06-20 12:58:32 +0200

    add timeline and slider

--- src/pages/timeline.jsx
+++ src/pages/timeline.jsx
@@ -1,6 +1,20 @@
 import Layout from "../components/Layout";
+import { RangeSliderContainer as RangeSlider } from "../components/RangeSlider";
+import { TimelineContainer as Timeline } from "../components/TimeLine";
 import styles from "../styles/pages/TimelinePage.module.scss";
 
 export default function TimelinePage() {
-  return <Layout>Timeline</Layout>;
+  return (
+    <Layout
+      toolbar={
+        <div>
+          <RangeSlider />
+        </div>
+      }
+    >
+      <div className={styles.timelineContainer}>
+        <Timeline />
+      </div>
+    </Layout>
+  );
 }
