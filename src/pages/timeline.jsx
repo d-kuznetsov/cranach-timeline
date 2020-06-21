aeee19a287c1b608998ed6bc26f1218b6ee672b2
Dmitry Kuznetsov <ds.kuznetsov93@yandex.ru>
2020-06-21 13:29:05 +0200

    add Viewer

--- src/pages/timeline.jsx
+++ src/pages/timeline.jsx
@@ -5,6 +5,7 @@ import { CategoryContainer as Categories } from "../components/Categories";
 import { LineHeightSliderContainer as LineHeightSlider } from "../components/LineHeightSlider";
 import { LegendContainer as Legend } from "../components/Legend";
 import { TimelineContainer as Timeline } from "../components/TimeLine";
+import { ViewContainer as Viewer } from "../components/Viewer";
 import styles from "../styles/pages/TimelinePage.module.scss";
 
 export default function TimelinePage() {
@@ -28,6 +29,7 @@ export default function TimelinePage() {
     >
       <div className={styles.timelineContainer}>
         <Timeline />
+        <Viewer />
       </div>
     </Layout>
   );
