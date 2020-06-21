aee1e4facb737caaa92979c6e981c1e3a7a7a4c7
Dmitry Kuznetsov <ds.kuznetsov93@yandex.ru>
2020-06-21 14:51:49 +0200

    add viewer open, implement line as component, use browser title

--- src/components/TimeLine/index.jsx
+++ src/components/TimeLine/index.jsx
@@ -1,14 +1,91 @@
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { setArtworkToView } from "../../redux/actions";
 
 import PropTypes from "prop-types";
-import Tooltip from "@material-ui/core/Tooltip";
 import styles from "./TimeLine.module.scss";
+import { getArtworkTitle, getPeriod } from "../../lib/extractArtworkData";
 import { CATEGORIES } from "../../constants";
 
 export function TimelineContainer() {
   const { period, artworksToView, lineHeight } = useSelector((state) => state);
-  return <TimelineComponent period={period} items={artworksToView} lineHeight={lineHeight} />;
+  const dispatch = useDispatch();
+  const handleLineClick = (artwork) => {
+    dispatch(setArtworkToView(artwork));
+  };
+  return (
+    <TimelineComponent
+      period={period}
+      items={artworksToView}
+      lineHeight={lineHeight}
+      onLineClick={handleLineClick}
+    />
+  );
 }
+
+export function TimelineComponent({ period, items, lineHeight = 16, onLineClick }) {
+  const yearList = getYearList(period);
+  const { modifiedItems, maxOffsetFactor } = withAdditionalProps(yearList, items);
+  const itemsByYear = getItemsByYear(modifiedItems);
+  const offset = lineHeight * 1.25;
+  return (
+    <div className={styles.container} style={{ height: offset * (maxOffsetFactor + 1) }}>
+      {yearList.map((year) => (
+        <div key={year} data-year={year} className={styles.year}>
+          {!itemsByYear[year]
+            ? null
+            : itemsByYear[year]
+                .filter((item) => item.periodLength)
+                .map((artwork) => {
+                  return (
+                    <ArtworkLine
+                      key={artwork.offsetFactor}
+                      artworkData={artwork}
+                      offset={offset}
+                      lineHeight={lineHeight}
+                      onClick={onLineClick}
+                    />
+                  );
+                })}
+        </div>
+      ))}
+    </div>
+  );
+}
+
+TimelineComponent.propTypes = {
+  period: PropTypes.array,
+  items: PropTypes.array,
+  lineHeight: PropTypes.number,
+  onLineClick: PropTypes.func,
+};
+
+function ArtworkLine({ artworkData, offset, lineHeight, onClick }) {
+  const { offsetFactor, periodLength, categoryId } = artworkData;
+  const handleClick = () => {
+    onClick(artworkData);
+  };
+  return (
+    <div
+      title={`${getArtworkTitle(artworkData)} | ${getPeriod(artworkData)}`}
+      className={styles.item}
+      style={{
+        marginLeft: "1px",
+        top: `${offset * offsetFactor}px`,
+        width: `calc(${100 * periodLength}% - 2px)`,
+        height: `${lineHeight}px`,
+        backgroundColor: CATEGORIES[categoryId].mainColor,
+      }}
+      onClick={handleClick}
+    ></div>
+  );
+}
+
+ArtworkLine.propTypes = {
+  offset: PropTypes.number,
+  lineHeight: PropTypes.number,
+  artworkData: PropTypes.object,
+  onClick: PropTypes.func,
+};
 
 function getItemsByYear(items) {
   const itemsByYear = {};
@@ -66,53 +143,3 @@ function withAdditionalProps(yearList, items) {
     maxOffsetFactor,
   };
 }
-
-export function TimelineComponent({ period, items, lineHeight = 16 }) {
-  const yearList = getYearList(period);
-  const { modifiedItems, maxOffsetFactor } = withAdditionalProps(yearList, items);
-  const itemsByYear = getItemsByYear(modifiedItems);
-  const offset = lineHeight * 1.25;
-  return (
-    <div className={styles.container} style={{ height: offset * (maxOffsetFactor + 1) }}>
-      {yearList.map((year) => (
-        <div key={year} data-year={year} className={styles.year}>
-          {!itemsByYear[year]
-            ? null
-            : itemsByYear[year]
-                .filter((item) => item.periodLength)
-                .map(({ offsetFactor, periodLength, categoryId, images, dating }) => (
-                  <Tooltip
-                    key={offsetFactor}
-                    enterDelay={1000}
-                    enterNextDelay={1000}
-                    title={
-                      <div>
-                        <img src={images.sizes.xs.src} alt={"d"} />
-                        <div>{`${dating.begin}-${dating.end}`}</div>
-                      </div>
-                    }
-                  >
-                    <div
-                      data-key={offsetFactor}
-                      className={styles.item}
-                      style={{
-                        marginLeft: "1px",
-                        top: `${offset * offsetFactor}px`,
-                        width: `calc(${100 * periodLength}% - 2px)`,
-                        height: `${lineHeight}px`,
-                        backgroundColor: CATEGORIES[categoryId].mainColor,
-                      }}
-                    ></div>
-                  </Tooltip>
-                ))}
-        </div>
-      ))}
-    </div>
-  );
-}
-
-TimelineComponent.propTypes = {
-  period: PropTypes.array,
-  items: PropTypes.array,
-  lineHeight: PropTypes.number,
-};
