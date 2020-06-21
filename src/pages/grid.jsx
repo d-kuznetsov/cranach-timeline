cb43b76434972f20f7c3a68a6724298c01856f19
Dmitry Kuznetsov <ds.kuznetsov93@yandex.ru>
2020-06-21 18:40:01 +0200

    use PopupFilter

--- src/pages/grid.jsx
+++ src/pages/grid.jsx
@@ -1,4 +1,6 @@
 import Layout from "../components/Layout";
+import PopupFilter from "../components/PopupFilter";
+import { RangeSliderContainer as RangeSlider } from "../components/RangeSlider";
 import { LegendContainer as Legend } from "../components/Legend";
 import { GridContainer as Grid } from "../components/Grid";
 import styles from "../styles/pages/GridPage.module.scss";
@@ -7,7 +9,15 @@ export default function GridPage() {
   return (
     <Layout
       toolbar={
-        <div>
+        <div className={styles.toolbar}>
+          <div className={styles.filters}>
+            <section className={styles.rangeSlider}>
+              <RangeSlider />
+            </section>
+            <section className={styles.popupFilter}>
+              <PopupFilter />
+            </section>
+          </div>
           <Legend />
         </div>
       }
