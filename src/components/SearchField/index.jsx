f436ec88f0252000c59c2c5013a6c40330ac4ded
Dmitry Kuznetsov <ds.kuznetsov93@yandex.ru>
2020-06-22 02:17:36 +0200

    create SearchField component  v.0.1

---
+++ src/components/SearchField/index.jsx
@@ -0,0 +1,48 @@
+import { useSelector, useDispatch } from "react-redux";
+import { setTextToSearch } from "../../redux/actions";
+
+import PropTypes from "prop-types";
+import { useRef } from "react";
+import IconButton from "@material-ui/core/IconButton";
+import SearchIcon from "@material-ui/icons/Search";
+import InputBase from "@material-ui/core/InputBase";
+import debounce from "lodash.debounce";
+import styles from "./SearchField.module.scss";
+
+export function SearchFieldContainer() {
+  const initialText = useSelector((state) => state.textToSearch);
+  const dispatch = useDispatch();
+  const handleInputCompleted = (text) => {
+    dispatch(setTextToSearch(text));
+  };
+  return <SearchFieldComponent initialText={initialText} onInputCompleted={handleInputCompleted} />;
+}
+
+export function SearchFieldComponent({ initialText, onInputCompleted, delay = 2000 }) {
+  const input = useRef(null);
+  const handleInput = () => {
+    onInputCompleted(input.current.value);
+  };
+  return (
+    <div className={styles.container}>
+      <IconButton size="small">
+        <SearchIcon />
+      </IconButton>
+      <InputBase
+        placeholder="Search..."
+        defaultValue={initialText}
+        variant="outlined"
+        size="small"
+        fullWidth={true}
+        inputRef={input}
+        onChange={debounce(handleInput, delay)}
+      />
+    </div>
+  );
+}
+
+SearchFieldComponent.propTypes = {
+  initialText: PropTypes.string,
+  onInputCompleted: PropTypes.func,
+  delay: PropTypes.number,
+};
