56f54bbbb6531540846f61e37d0728890d0b1c3e
Dmitry Kuznetsov <ds.kuznetsov93@yandex.ru>
2020-06-23 00:02:15 +0200

    use memoization

--- src/components/Grid/index.jsx
+++ src/components/Grid/index.jsx
@@ -1,16 +1,26 @@
 import { useSelector } from "react-redux";
 
 import PropTypes from "prop-types";
-import { FixedSizeList as List } from "react-window";
+import { memo } from "react";
+import memoize from "memoize-one";
 import AutoSizer from "react-virtualized-auto-sizer";
+import { FixedSizeList as List, areEqual } from "react-window";
 import GridList from "@material-ui/core/GridList";
 import GridListTile from "@material-ui/core/GridListTile";
 import GridListTileBar from "@material-ui/core/GridListTileBar";
+import { getArtworkTitle, getImageSrc, getInvolvedPersons } from "../../lib/extractArtworkData";
 
 export function GridContainer() {
   const { artworksToView } = useSelector((state) => state);
   return <GridComponent items={artworksToView} columnNumber={4} space={8} />;
 }
+
+const createItemData = memoize((items, space, gridCellHeight, columnNumber) => ({
+  items,
+  space,
+  gridCellHeight,
+  columnNumber,
+}));
 
 export function GridComponent({ items, columnNumber, space }) {
   const rowCount = Math.ceil(items.length / columnNumber);
@@ -21,12 +31,7 @@ export function GridComponent({ items, columnNumber, space }) {
         const itemHeight = gridCellHeight + space;
         return (
           <List
-            itemData={{
-              items,
-              space,
-              gridCellHeight,
-              columnNumber,
-            }}
+            itemData={createItemData(items, space, gridCellHeight, columnNumber)}
             height={height}
             itemCount={rowCount}
             itemSize={itemHeight}
@@ -47,17 +52,21 @@ GridComponent.propTypes = {
   space: PropTypes.number,
 };
 
-const Row = ({ index: i, data, style }) => {
+const Row = memo(({ index: i, data, style }) => {
   const { items, space, gridCellHeight, columnNumber } = data;
   const columns = [];
   for (let j = i * columnNumber; j < i * columnNumber + columnNumber; j++) {
     items[j] &&
       columns.push(
         <GridListTile key={items[j].objectId}>
-          <img src={items[j].images.sizes.s.src} alt={"dsd"} />
+          <img
+            src={getImageSrc(items[j])}
+            alt={getArtworkTitle(items[j])}
+            onClick={console.log("aaa")}
+          />
           <GridListTileBar
-            title={items[j].titles[0] && items[j].titles[0].title}
-            subtitle={items[j].involvedPersons[0] && items[j].involvedPersons[0].name}
+            title={getArtworkTitle(items[j])}
+            subtitle={getInvolvedPersons(items[j])}
           />
         </GridListTile>
       );
@@ -69,7 +78,7 @@ const Row = ({ index: i, data, style }) => {
       </GridList>
     </div>
   );
-};
+}, areEqual);
 
 Row.propTypes = {
   index: PropTypes.number,
