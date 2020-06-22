3330968536994d33583b6bf51077037463c166af
Dmitry Kuznetsov <ds.kuznetsov93@yandex.ru>
2020-06-22 20:03:02 +0200

    add filters to popup panel

--- src/pages/grid.jsx
+++ src/pages/grid.jsx
@@ -2,8 +2,10 @@ import Layout from "../components/Layout";
 import { SearchFieldContainer as SearchField } from "../components/SearchField";
 import PopupFilter from "../components/PopupFilter";
 import { RangeSliderContainer as RangeSlider } from "../components/RangeSlider";
+import { CategoryContainer as Categories } from "../components/Categories";
 import { LegendContainer as Legend } from "../components/Legend";
 import { GridContainer as Grid } from "../components/Grid";
+import Typography from "@material-ui/core/Typography";
 import styles from "../styles/pages/GridPage.module.scss";
 
 export default function GridPage() {
@@ -17,7 +19,12 @@ export default function GridPage() {
               <SearchField />
             </section>
             <section className={styles.popupFilter}>
-              <PopupFilter />
+              <PopupFilter>
+                <Typography>Date range</Typography>
+                <RangeSlider base={true} />
+                <Typography>Categories</Typography>
+                <Categories />
+              </PopupFilter>
             </section>
           </div>
           <Legend />
