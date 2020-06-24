e93e47c291e56962508d363d4d47c76e2740914c
Dmitry Kuznetsov <ds.kuznetsov93@yandex.ru>
2020-06-24 14:37:40 +0200

    add filter line, improve styles

--- src/components/Legend/index.jsx
+++ src/components/Legend/index.jsx
@@ -1,33 +1,61 @@
 import { useSelector } from "react-redux";
 
 import PropTypes from "prop-types";
-import Typography from "@material-ui/core/Typography";
+import DateRangeIcon from "@material-ui/icons/DateRange";
+import CategoryIcon from "@material-ui/icons/Category";
+import SearchIcon from "@material-ui/icons/Search";
 import styles from "./Legend.module.scss";
 
 export function LegendContainer() {
-  const { categories, period } = useSelector((state) => state);
-  return <LegendComponent categories={categories} period={period} />;
+  const { categories, period, textToSearch, artworksToView } = useSelector((state) => state);
+  return (
+    <LegendComponent
+      categories={categories}
+      period={period}
+      searchText={textToSearch}
+      itemNumber={artworksToView.length}
+    />
+  );
 }
 
-export function LegendComponent({ categories, period }) {
+export function LegendComponent({ categories, period, searchText, itemNumber }) {
   const [start, end] = period;
   return (
     <div className={styles.container}>
-      <section className={styles.categories}>
-        {Object.keys(categories).map((key) => {
-          const { id, mainColor, label } = categories[key];
-          return (
-            <div key={id} className={styles.category}>
-              <div className={styles.categoryColor} style={{ backgroundColor: mainColor }}></div>
-              <Typography>{label}</Typography>
-            </div>
-          );
-        })}
-      </section>
-      <section className={styles.period}>
-        <Typography>
-          {start} - {end}
-        </Typography>
+      <section className={styles.results}>{itemNumber} results for</section>
+      <section className={styles.filters}>
+        <section className={styles.period}>
+          <DateRangeIcon color="action" className={styles.periodIcon} />
+          <div className={styles.periodContent}>
+            {start}-{end}
+          </div>
+        </section>
+        <section className={styles.categories}>
+          <CategoryIcon color="action" className={styles.categoriesIcon} />
+          <div className={styles.categoriesContent}>
+            {Object.keys(categories).map((key) => {
+              const { id, mainColor, label, displayed } = categories[key];
+              if (!displayed) {
+                return null;
+              }
+              return (
+                <div key={id} className={styles.category}>
+                  <div
+                    className={styles.categoryColor}
+                    style={{ backgroundColor: mainColor }}
+                  ></div>
+                  <div className={styles.categoryLabel}>{label}</div>
+                </div>
+              );
+            })}
+          </div>
+        </section>
+        {searchText && (
+          <section className={styles.search}>
+            <SearchIcon color="action" className={styles.searchIcon} />
+            <div className={styles.searchContent}>{searchText}</div>
+          </section>
+        )}
       </section>
     </div>
   );
@@ -36,4 +64,6 @@ export function LegendComponent({ categories, period }) {
 LegendComponent.propTypes = {
   categories: PropTypes.object,
   period: PropTypes.array,
+  searchText: PropTypes.string,
+  itemNumber: PropTypes.number,
 };
