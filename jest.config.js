bb5feb56d707fb175c52b2f6a2a73bbfb264692e
Dmitry Kuznetsov <ds.kuznetsov93@yandex.ru>
2021-04-03 22:43:33 +0200

    update testing settings

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,7 @@
 module.exports = {
   testPathIgnorePatterns: ["<rootDir>/.next/", "<rootDir>/node_modules/"],
   setupFilesAfterEnv: ["<rootDir>/setupTests.js"],
-  transform: {
-    "^.+\\.(js|jsx|ts|tsx)$": "<rootDir>/node_modules/babel-jest",
+  moduleNameMapper: {
     "\\.(css|less|scss|sass)$": "identity-obj-proxy",
   },
 };
